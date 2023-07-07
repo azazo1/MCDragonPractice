1abd9dc46964080fe100c7fdf0f8ebdc7811a14d
azazo1 <azazo1@qq.com>
2023-07-07 08:54:15 +0800

    濒死血量时，末影龙迅速飞升，号召末影人，增加火球发射频率 现在玩家摔落伤害也被记录

--- src/main/java/com/azazo1/dragonpractice/progress/Fighting.java
+++ src/main/java/com/azazo1/dragonpractice/progress/Fighting.java
@@ -1,6 +1,7 @@
 package com.azazo1.dragonpractice.progress;
 
 import com.azazo1.dragonpractice.MyLog;
+import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
@@ -12,12 +13,11 @@ import org.bukkit.boss.BossBar;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.EnderDragon;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -25,11 +25,11 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -41,16 +41,22 @@ public class Fighting extends Progress implements Listener, CommandExecutor {
     protected static final int healthIncreasePerDeath = 50; // 玩家每次死亡末影龙增加的血量
     protected static final int maxPlayerLife = 5; // 玩家最大生命数量
     protected static final int sucWaitingSeconds = 30; // 玩家对局成功后在末地等待的时间
+    protected static final int irritateEnderManHealth = 10; // 末影龙濒危血量阈值，末影龙在低于该血量时会号召所有末影人让他们朝玩家攻击
+    protected static final int irritateEnderManAmountPerPlayer = 5; // 对于每个玩家，末影龙号召末影人的数量
+    protected static final int fireBallIntervalSeconds = 5; // 末影龙在濒危血量下每发火球发射间隔时间
+    protected static final int fireBallVelocity = 5; // 末影龙在濒危血量下发射的火球的飞行速度 m/s
     protected HashMap<Player, Integer> playerLife = new HashMap<>(); // 玩家剩余生命数量
     protected HashMap<Player, Double> playerDamageToEnderDragon = new HashMap<>(); // 玩家对末影龙造成的积累伤害
     protected HashMap<Player, Double> playerGetDamage = new HashMap<>(); // 玩家受伤统计
     protected final AtomicBoolean alive = new AtomicBoolean(true);
     protected final AtomicBoolean fightOver = new AtomicBoolean(false); // 对战是否结束（即胜负是否已分）
+    protected final AtomicBoolean enderManIrritated = new AtomicBoolean(false); // 是否惹怒了末影人（一场对局末影龙发动一次）
     protected World endWorld; // 临时创建的末地
     protected EnderDragon enderDragon; //末影龙
     protected Location spawnLocationInOverWorld; // 主世界的重生点
     protected double enderDragonMaxHealth; // 末影龙最大血量
     protected FightResult result;
+    protected HashSet<Enderman> freeOfDragonBreath = new HashSet<>(); // 免受龙息伤害的末影人（末影龙号召的末影人不会受龙息伤害）
 
     public Fighting(JavaPlugin plugin) {
         super(plugin);
@@ -99,9 +105,72 @@ public class Fighting extends Progress implements Listener, CommandExecutor {
                 // 找不到末影龙，说明末影龙消失了，即死亡了
                 MyLog.i("末影龙被神秘力量杀死！");
                 onFightEnd(true);
+            } else {
+                if (dragon.getHealth() < irritateEnderManHealth && !enderManIrritated.get()) {
+                    irritateEnderMan();
+
+                    Location location = enderDragon.getLocation();
+                    location.setY(location.getY() + 50);
+                    enderDragon.teleport(location);
+                    MyLog.i("末影龙迅速飞升");
+                    MyLog.i("末影龙发射火球频率增加，每 %d 秒发射一次".formatted(fireBallIntervalSeconds));
+                    fireBallFrequently();
+                }
             }
         }
         Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, this::update, 20);
+    }
+
+    /**
+     * 末影龙在极低生命值时会增加发射火球的频率
+     */
+    protected void fireBallFrequently() {
+        if (!alive.get()) {
+            MyLog.e("本 Fighting 状态已结束");
+            return;
+        }
+        // 随机选择一个玩家
+        Player randomChosen = (Player) playerLife.keySet().toArray()[new Random().nextInt(0, playerLife.size())];
+        Location endLocation = randomChosen.getLocation();
+        Location startLocation = enderDragon.getEyeLocation();
+        // 取两个location的中点 防止火球被末影龙自己干掉
+        Location middleLocation = new Location(endLocation.getWorld(), (endLocation.getX() + startLocation.getX()) / 2, (endLocation.getY() + startLocation.getY()) / 2, (endLocation.getZ() + startLocation.getZ()) / 2);
+        Vector fireVector = new Vector(endLocation.getX() - startLocation.getX(), endLocation.getY() - startLocation.getY(), endLocation.getZ() - startLocation.getZ()).normalize();
+        middleLocation.setDirection(fireVector);
+        // 发射
+        DragonFireball ball = (DragonFireball) endWorld.spawnEntity(middleLocation, EntityType.DRAGON_FIREBALL);
+        ball.setVelocity(fireVector.multiply(fireBallVelocity));
+        ball.setShooter(enderDragon);
+        ball.setIsIncendiary(false);
+        ball.setFireTicks(0);
+        enderDragon.playSound(Sound.sound(org.bukkit.Sound.ENTITY_ENDER_DRAGON_AMBIENT, Sound.Source.AMBIENT, 640, 1));
+        MyLog.i("末影龙发射火球！");
+        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, this::fireBallFrequently, 20 * fireBallIntervalSeconds);
+    }
+
+    /**
+     * 末影龙进入超残血状态：
+     * 1. 号召一定数量的末影人
+     * 2. 取消栖息
+     * 3. 增大喷火球的频率
+     */
+    protected void irritateEnderMan() {
+        enderManIrritated.set(true);
+        Collection<Enderman> enderMen = endWorld.getEntitiesByClass(Enderman.class);
+        int expectedToCallAmount = playerLife.size() * irritateEnderManAmountPerPlayer;
+        int calledAmount = 0;
+        for (Enderman enderman : enderMen) {
+            if (calledAmount >= expectedToCallAmount) {
+                break;
+            }
+            Player randomChosen = (Player) playerLife.keySet().toArray()[new Random().nextInt(0, playerLife.size())];
+            enderman.setScreaming(true);
+            enderman.setHasBeenStaredAt(true);
+            enderman.setTarget(randomChosen);
+            freeOfDragonBreath.add(enderman); // 不受龙息伤害的末影人
+            calledAmount++;
+        }
+        MyLog.i("末影龙号召 %d 个末影人攻击玩家！".formatted(calledAmount));
     }
 
     @Override
@@ -307,7 +376,26 @@ public class Fighting extends Progress implements Listener, CommandExecutor {
     }
 
     /**
-     * 统计玩家对末影龙造成的伤害, 统计玩家收到的伤害
+     * 统计玩家收到的伤害<br>
+     * 消除被号召末影人收到龙息的伤害
+     */
+    @EventHandler
+    public void onEntityDamageEvent(@NotNull EntityDamageEvent e) {
+        if (!alive.get()) {
+            return;
+        }
+        double damage = e.getDamage();
+        if (e.getEntity() instanceof Player player && playerGetDamage.containsKey(player)) {
+            playerGetDamage.replace(player, playerGetDamage.get(player) + damage);
+            double health = player.getHealth() - damage; // 这里似乎血量时延迟改变的这一帧仍然是扣血前的血量
+            MyLog.i("玩家 %s 收到 %d 点伤害, 剩余血量: %d".formatted(player.getName(), (int) damage, (int) health));
+        } else if (e.getEntity() instanceof Enderman enderman && freeOfDragonBreath.contains(enderman)) {
+            e.setCancelled(true);
+        }
+    }
+
+    /**
+     * 统计玩家对末影龙造成的伤害
      */
     @EventHandler
     public void onEntityDamageByEntityEvent(@NotNull EntityDamageByEntityEvent e) {
@@ -327,10 +415,6 @@ public class Fighting extends Progress implements Listener, CommandExecutor {
             } else {
                 MyLog.i("神秘力量对末影龙造成 %d 点伤害, 末影龙剩余血量: %d".formatted((int) damage, (int) health));
             }
-        } else if (e.getEntity() instanceof Player player && playerGetDamage.containsKey(player)) {
-            playerGetDamage.replace(player, playerGetDamage.get(player) + damage);
-            double health = player.getHealth() - damage; // 这里似乎血量时延迟改变的这一帧仍然是扣血前的血量
-            MyLog.i("玩家 %s 收到 %d 点伤害, 剩余血量: %d".formatted(player.getName(), (int) damage, (int) health));
         }
     }
 
